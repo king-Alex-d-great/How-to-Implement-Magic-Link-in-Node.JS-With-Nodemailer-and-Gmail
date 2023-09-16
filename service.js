e839a7b6757fed62345b1c7e129b06147f447f72
king-Alex-d-great <ogubuikealex@gmail.com>
2023-09-16 02:22:14 +0100

    Feat: Create helper methods

---
+++ service.js
@@ -0,0 +1,33 @@
+const nodeMailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+
+export const getTransport = () => nodeMailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+export const generateToken = (email) => {
+    const expirationDate = new Date();
+    expirationDate.setMinutes(new Date().getMinutes() + 45);
+    return jwt.sign({ email, expirationDate }, process.env.JWT_SECRET_KEY);
+};
+
+export const getMailOptions = (email, link) => {
+    let body = `
+  <h2>Hey ${email}</h2>
+  <p>Here's the special magic link you requested:</p>
+  <p>${link}</p>
+  <p>Please note that for added security this link becomes invalid after 45 minutes</p>
+  <p>Stay Jiggy</p>`;
+
+    return {
+        body,
+        subject: "Urgent: Super Secret Magic Link",
+        to: email,
+        html: body,
+        from: process.env.EMAIL_ADDRESS,
+    };
+};
